1dc7bc2dda88970225fb8aad91e1ef4dd0030a70
Jeremy Bass <jeremybass@cableone.net>
2016-07-06 13:08:28 -0700

    make sure that we have searched for it first and cleaned up the yum too

--- platform-bootstrap.sh
+++ platform-bootstrap.sh
@@ -27,8 +27,11 @@ ssh-keyscan -H 192.30.252.131 >> ~/.ssh/known_hosts
 ssh-keygen -R github.com
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
+yum clean all
 yum -y install unzip
-yum install kernel-headers  --disableexcludes=all
+
+yum search kernel-headers  --disableexcludes=all
+yum -y install kernel-headers  --disableexcludes=all
 
 mkdir -p /var/www
 
