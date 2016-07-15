bfcfc173151dc255c54af4a38846a0f0a32c8fb7
Jeremy Bass <jeremybass@cableone.net>
2016-07-14 20:50:52 -0700

    match what is in the script said to be used

--- platform-bootstrap.sh
+++ platform-bootstrap.sh
@@ -55,18 +55,25 @@ gitploy ls 2>&1 | grep -qi "platform" && gitploy up platform && gitploy re platf
 gitploy ls 2>&1 | grep -qi "platform" || gitploy clone -b master platform git@github.com:jeremyBass/wsu-platform-parts.git
 
 
-    cd /tmp && rm -fr wsu-web
-    cd /tmp && curl -o wsu-web.zip -L https://github.com/washingtonstateuniversity/wsu-web-provisioner/archive/master.zip
-    cd /tmp && unzip wsu-web.zip
-    cd /tmp && mv WSU-Web-Provisioner-master wsu-web
-    cp -fr /tmp/wsu-web/provision/salt /srv/
-    cp /tmp/wsu-web/provision/salt/config/local.yum.conf /etc/yum.conf
-    rpm -Uvh --force http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    sed -i 's/mirrorlist=https/mirrorlist=http/' /etc/yum.repos.d/epel.repo
-    yum -y update python
-    sh /tmp/wsu-web/provision/bootstrap_salt.sh -K stable
-    rm /etc/salt/minion.d/*.conf
-    rm /etc/salt/minion_id
-    echo "wsuwp-prod" > /etc/salt/minion_id
-    cp /tmp/wsu-web/provision/salt/minions/wsuwp.conf /etc/salt/minion.d/
-    salt-call --local --log-level=info --config-dir=/etc/salt state.highstate
+mkdir -p /srv/pillar/
+mkdir -p /srv/pillar/config/
+touch /srv/pillar/top.sls
+touch /srv/pillar/network.sls
+touch /srv/pillar/mysql.sls
+
+groupadd ucadmin
+useradd ucadmin -m -g ucadmin
+passwd ucadmin
+visudo
+
+cd /tmp && rm -fr wsu-web
+cd /tmp && curl -o wsu-web.zip -L https://github.com/washingtonstateuniversity/wsu-web-provisioner/archive/master.zip
+cd /tmp && unzip wsu-web.zip
+cd /tmp && mv WSU-Web-Provisioner-master wsu-web
+cp -fr /tmp/wsu-web/provision/salt /srv/
+cp /tmp/wsu-web/provision/salt/config/yum.conf /etc/yum.conf
+
+sh /tmp/wsu-web/provision/bootstrap_salt.sh -K stable
+cp /tmp/wsu-web/provision/salt/minions/wsu-general.conf /etc/salt/minion.d/
+
+salt-call --local --log-level=info --config-dir=/etc/salt state.highstate
